789ab019a9ae028c3c7d197dca206dc3795f52a4
trinisic <37811191+trinisic@users.noreply.github.com>
2018-03-26 14:34:24 -0400

    TwBotv2

--- vs.py
+++ vs.py
@@ -29,21 +29,21 @@ prinf('AskPrice:{}'.format(ask))
 #bid = driver.find_element_by_class_name('market-summary-bid-price').text
 #border = driver.find_element_by_class_name('sell-amount').text
 
-bbtc = float(bid) * float(border)
-bbtc = round(bbtc,8)
+#bbtc = float(bid) * float(border)
+#bbtc = round(bbtc,8)
 #driver.quit()  # ブラウザーを終了する
 ask = float(ask) * 100000000
 bid = float(bid) * 100000000
 ask = int(round(ask))
 bid = int(round(bid))
 
-consumer_key        = 'UEb18HBscs2h9Mhpe6KmsCTbz'
-consumer_secret     = 'r2ByglOgZm326rHXfAAG4J1SiF9KoEraKzMGKAbZFcBIcvQDye'
-access_token        = '113384111-05QVgLnhO23Z3cSP7OqjrM8skjzgS96tTncTGecY'
-access_token_secret = 'if0egdApemMhw64LaLJ8Eh3AOdN5GayOeKFfCdxQitzhQ'
+#consumer_key        = 'UEb18HBscs2h9Mhpe6KmsCTbz'
+#consumer_secret     = 'r2ByglOgZm326rHXfAAG4J1SiF9KoEraKzMGKAbZFcBIcvQDye'
+#access_token        = '113384111-05QVgLnhO23Z3cSP7OqjrM8skjzgS96tTncTGecY'
+#access_token_secret = 'if0egdApemMhw64LaLJ8Eh3AOdN5GayOeKFfCdxQitzhQ'
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
-api = tweepy.API(auth_handler=auth)
-url = 'https://goo.gl/aUT2E3'
-api.update_status(status = "NANJCOIN板の情報をお知らせするやで〜\n"+"売:" + str(ask) +"satoshi  " \n                    VS\n買:" + str(bid) + "satoshi やで！\n詳しくはCEをチェックや！ほな、また！\n#NANJCOIN\n" + url)
+#auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+#auth.set_access_token(access_token, access_token_secret)
+#api = tweepy.API(auth_handler=auth)
+#url = 'https://goo.gl/aUT2E3'
+#api.update_status(status = "NANJCOIN板の情報をお知らせするやで〜\n"+"売:" + str(ask) +"satoshi  " \n                    VS\n買:" + str(bid) + "satoshi やで！\n詳しくはCEをチェックや！ほな、また！\n#NANJCOIN\n" + url)
