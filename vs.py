79e97b017d68adf1ffcaa4c85b47d86cfa29f251
trinisic <37811191+trinisic@users.noreply.github.com>
2018-03-26 20:36:24 -0400

    t1.0.0

--- vs.py
+++ vs.py
@@ -19,10 +19,10 @@ bid = int(round(bid))
 print('BidPrice: {} sat'.format(bid))
 print('AskPrice: {} sat'.format(ask))
 ind = 0
-for ind in 50:
+for ind in range(50):
     print("{}/n".format(data2['result']['SellOrders'][ind]))
     ind = ind + 1
-    
+
 #st0 = datetime.datetime(2017, 3, 16, 0, 0, 0)
 #int( time.mktime( datetime.datetime.strptime( str(st0), "%Y-%m-%d %H:%M:%S" ).timetuple() ) )
 #for st1 in data2['result']['SellOrders']['OrderTime']:
