18d920b592761aa070b545a3167dfb11f364847a
trinisic <37811191+trinisic@users.noreply.github.com>
2018-03-26 21:04:35 -0400

    t1.1.0

--- vs.py
+++ vs.py
@@ -20,15 +20,20 @@ print('BidPrice: {} sat'.format(bid))
 print('AskPrice: {} sat'.format(ask))
 ind = 0
 st0 = datetime.datetime(2017, 3, 16, 0, 0, 0)
+bt0 = datetime.datetime(2017, 3, 16, 0, 0, 0)
 for ind in range(50):
 #    print("{}".format(data2['result']['SellOrders'][ind]))
 #    for i in range(len(data2['result']['SellOrders'])):
 #    print("{}".format(data2['result']['SellOrders'][ind]['OrderTime']))
     st1 = data2['result']['SellOrders'][ind]['OrderTime']
     st1 = datetime.datetime.strptime(str(st1), "%Y-%m-%d %H:%M:%S" )
+    bt1 = data2['result']['BuyOrders'][ind]['OrderTime']
+    bt1 = datetime.datetime.strptime(str(bt1), "%Y-%m-%d %H:%M:%S" )
     if st1 > st0: st0 = st1
+    if bt1 > bt0: bt0 = bt1
     ind = ind + 1
 print("{}".format(str(st1)))
+print("{}".format(str(bt1)))
 #int( time.mktime( datetime.datetime.strptime( str(st0), "%Y-%m-%d %H:%M:%S" ).timetuple() ) )
 #for st1 in data2['result']['SellOrders']['OrderTime']:
 #	int( time.mktime( datetime.datetime.strptime( str(st1), "%Y-%m-%d %H:%M:%S" ).timetuple() ) )
