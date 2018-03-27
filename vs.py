1bd37facaa0dcbdbd1fa63ba05917ea460f33af4
trinisic <37811191+trinisic@users.noreply.github.com>
2018-03-26 20:55:30 -0400

    t1.0.0

--- vs.py
+++ vs.py
@@ -19,13 +19,15 @@ bid = int(round(bid))
 print('BidPrice: {} sat'.format(bid))
 print('AskPrice: {} sat'.format(ask))
 ind = 0
+st0 = datetime.datetime(2017, 3, 16, 0, 0, 0)
 for ind in range(50):
-    print("{}".format(data2['result']['SellOrders'][ind]))
+#    print("{}".format(data2['result']['SellOrders'][ind]))
 #    for i in range(len(data2['result']['SellOrders'])):
-    print("{}".format(data2['result']['SellOrders'][ind]['OrderTime']))
+#    print("{}".format(data2['result']['SellOrders'][ind]['OrderTime']))
+    st1 = data2['result']['SellOrders'][ind]['OrderTime']
+    if st1 > st0: st0 = st1
     ind = ind + 1
-
-#st0 = datetime.datetime(2017, 3, 16, 0, 0, 0)
+print("{}".format(str(st1)))
 #int( time.mktime( datetime.datetime.strptime( str(st0), "%Y-%m-%d %H:%M:%S" ).timetuple() ) )
 #for st1 in data2['result']['SellOrders']['OrderTime']:
 #	int( time.mktime( datetime.datetime.strptime( str(st1), "%Y-%m-%d %H:%M:%S" ).timetuple() ) )
